26593b4465c0ccb93234ecd4b392ff51f86e27e1
Saidkodirov <stolibjon123@gmail.com>
2023-12-29 22:12:26 +0500

    I wrote 2-lessons code

---
+++ lesson2/sandbox.ts
@@ -0,0 +1,12 @@
+const character = "mario";
+
+console.log(character);
+
+const inputs = document.querySelectorAll("input");
+
+console.log(inputs);
+
+inputs.forEach((el) => {
+    console.log(el);
+    
+})
