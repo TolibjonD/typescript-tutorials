7adcb138b85a91c0b1c1de74e37b85054f5e986c
Saidkodirov <stolibjon123@gmail.com>
2024-01-03 20:12:11 +0500

    I learned arrays and objects in lesson4

---
+++ codes/sandbox.ts
@@ -0,0 +1,29 @@
+// ARRAYS
+
+let names = ['luigi' , 'mario', 'yoshi'];
+
+names.push('toad');
+// names.push(3);
+let numbers = [1,2,3,4,5];
+numbers.push(21);
+
+// numbers.push("shaun");
+// numbers[1] = "Shaun";
+
+let mixed = ['ken', 4, "chun-li", 8, 9];
+
+mixed.push("ryu");
+mixed.push(10)
+mixed[0] = 3;
+
+// OBJECTS  
+
+let ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30
+}
+
+ninja.age = 40;
+ninja.name = "Said"
+// ninja.age = "30";
